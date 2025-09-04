b845b16eeb0f6ef1daca9f06ed665e3c1ab6db0b
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2025-09-04 19:46:22 +0200

    create grid with js factorie function

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ const player1 = createPlayer("Hubert", "X")
 const player2 = createPlayer("Dieter","O")
 
 console.log(player1, player2)
+
+function createGameboard () {
+    const rows = 4; 
+    const columns = 4; 
+    const board = [];
+    
+    for (let r = 0 ; r < rows; r++) {
+        board[r] =[];
+        for (let c = 0; c < columns; c++){
+            board[r].push(0);
+        }
+    }
+
+    return {board};
+};
+
+console.log(createGameboard().board)
+
+function markField () {
+
+}
+
+function gameController() {
+    
+}
