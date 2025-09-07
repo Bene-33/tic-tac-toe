fb19dc56d67bae76dfcf915b5b31deb66c5dd618
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2025-09-07 15:19:16 +0200

    added setMarker functionallity

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function createPlayer (){
+function createPlayer (name, symbol) {
+    const playerName = name;
+    const playerSymbol = symbol;
 
+    return{playerName, playerSymbol};
 };
 
-function Gameboard () {
+function gameboard () {
 
     const rows = 3; 
     const columns = 3; 
@@ -11,21 +14,62 @@ function Gameboard () {
     for (let r = 0; r < rows; r++) {
         board[r] = [];
         for (let c = 0; c < columns; c++){
-            board[r].push(" ");
+            board[r].push(cell());
         };
     };
 
 
-    function markCell (){
-        //identify empty Cells
+    function markCell (row, column, symbol){
+        const availableCell = () => {
+            if(board[row][column].getValue() === "") {
+                return true
+            }
+            else {
+                return false
+            };
+        };
+        
+        if(availableCell()){
+            board[row][column].addMarker(symbol);
+        };
+    };
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues)
     };
 
-    return {board, markCell}
+    return {
+        board, 
+        markCell,
+        printBoard}
+};
+
+function cell() {
+    let value = "";
+
+    const addMarker = (player) =>{
+        value = player;
+    };
+
+    const getValue =() => value;
+
+    return{
+        addMarker,
+        getValue
+
+    };
+
+}
+
+function gameController (){
     
 };
 
-console.log(Gameboard().board);
-
-function gameController (){
-
-};
+console.log(createPlayer("Josh", "X"));
+console.log(createPlayer("Alan", "O"));
+const game = gameboard()
+game.printBoard();
+game.markCell(1,1,"X")
+game.printBoard();
+game.markCell(1,1,"O");
+game.printBoard();
