302b43765f880622a27bec72fb49fb45ccdc4a97
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2025-10-29 22:07:09 +0100

    added some formatting

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function createPlayer (name, symbol) {
+
     const playerName = name;
     const playerSymbol = symbol;
 
@@ -15,6 +16,7 @@ function gameboard () {
 
     for (let r = 0; r < rows; r++) {
         board[r] = [];
+
         for (let c = 0; c < columns; c++){
             board[r].push(cell());
         };
@@ -23,27 +25,57 @@ function gameboard () {
     const getBoard = () => board;
 
     function markCell (row, column, symbol){
+        
         const availableCell = () => {
+           
             if(board[row][column].getValue() === "") {
+ 
                 return true
             }
+
             else {
+ 
                 return false
             };
         };
+
         if(availableCell()){
+ 
             board[row][column].addMarker(symbol);
+           
             roundCount++;
+ 
             return true;
         };
+
         return false;
     };
 
+    function cell() {
+     
+        let value = "";
+        
+        const addMarker = (player) => {
+            value = player;
+     
+        };
+        
+        const getValue =() => value;
+        
+        return{
+            addMarker,
+            getValue
+        };
+    };
+
     const printBoard = () => {
+        
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
         console.log(boardWithCellValues)
     };
+
     function checkWinCondition(board) {
+      
         const winPatterns = [
             //row
             [[0,0] , [0,1] , [0,2]],
@@ -57,6 +89,7 @@ function gameboard () {
             [[0,0] , [1,1] , [2,2]],
             [[0,2] , [1,1] , [2,0]]
         ];
+
         for (const pattern of winPatterns){
             const [a, b, c] = pattern; 
             const cellA = board[a[0]][a[1]].getValue();
@@ -64,33 +97,45 @@ function gameboard () {
             const cellC = board[c[0]][c[1]].getValue();
 
             if(cellA !== "" && cellA === cellB && cellB === cellC){
+              
                 return cellA;
             }
         }
+
         return null;
     };
 
     function checkDrawCondition(){
         if(maxRounds === roundCount){
+
             return true
         }
     };
+
     const printWinCondition = () => {
+        
         const winner = checkWinCondition(board);
         const  draw = checkDrawCondition(board);
+        
         if(winner) {
+        
             let winnerName = "";
+        
             if (winner === playerOne.playerSymbol){
                 winnerName = playerOne.playerName;
             }
+
             else if (winner === playerTwo.playerSymbol){
                 winnerName = playerTwo.playerName;
             }
+
             console.log(`The winner is ${winnerName}`);
         }
+
         else if (draw){
-            console.log("draw");
-        }  
+            console.log("It's a draw");
+        }
+
         else {
             return null;
          }
@@ -105,17 +150,6 @@ function gameboard () {
         
 };
 
-function cell() {
-    let value = "";
-    const addMarker = (player) => {
-        value = player;
-    };
-    const getValue =() => value;
-    return{
-        addMarker,
-        getValue
-    };
-};
 
 
 
@@ -126,6 +160,7 @@ function gameController (){
 const playerOne = createPlayer("Josh", "X");
 const playerTwo = createPlayer("Alan", "O");
 const game = gameboard();
+// play rounds
 game.markCell(0,0,playerTwo.playerSymbol);
 game.markCell(0,1,playerOne.playerSymbol);
 game.markCell(0,2,playerOne.playerSymbol);
